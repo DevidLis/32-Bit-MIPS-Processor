3bb38b00c1cf46dc74040c8b2f80ecaab326779c
Ubuntu <lis.devid@gmail.com>
2020-08-09 22:04:58 +0300

    Added DFF module and TB

---
+++ DFF.v
@@ -0,0 +1,11 @@
+module dff(Q, D, clk, resetb);
+    input D, resetb, clk;
+    output reg Q;
+
+    always @(posedge clk or negedge resetb) 
+    begin
+        if (~resetb) Q <= 1'b0;
+        else Q <= D;
+    end
+endmodule
+
---
+++ DFF_tb.v
@@ -0,0 +1,37 @@
+`timescale 1us/1ns
+`include "DFF.v"
+
+module dff_tb();
+    reg D; reg resetb; reg clk;
+    wire Q;
+
+    dff idff(Q, D, clk, resetb);
+
+    initial begin 
+        clk <= 1'b0;
+        resetb <= 1'b1;
+        D <= 1'b0;
+    end
+
+    always begin 
+        forever #1 clk = ~clk;
+    end
+
+    initial begin
+        $monitor($time, " Clock: %b, D: %b, resetb: %b, Q: %b", 
+                        clk, D, resetb, Q);
+        $dumpfile("DFF.vcd");
+        $dumpvars(0, dff_tb);
+        #5;
+        D <= 1'b1;
+        #5;
+        D <= 1'b0;
+        #5;
+        resetb <= 1'b0;
+        #5;
+        D <= 1'b1;
+        #5;
+        $stop;
+
+    end
+endmodule
